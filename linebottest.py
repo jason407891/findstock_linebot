b73e2b3307c3ebe16cb13ae2a4b0a241cbdd188e
jason.lin <jason407891@gmail.com>
2023-08-19 17:08:17 +0800

    try

--- linebottest.py
+++ linebottest.py
@@ -56,7 +56,7 @@ def quote(event):
 
                 line_bot_api.reply_message(
                     event.reply_token,
-                    TextSendMessage(text=sendmsg)
+                    TextSendMessage(text=event.message.text)
                 )
 
 if __name__ == "__main__":
